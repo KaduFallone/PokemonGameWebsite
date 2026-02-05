0f20c646bd112e099966610497d0f39f4ca5b61a
KaduFallone <kadufallone@gmail.com>
2026-02-05 10:04:14 -0300

    feat: add pagina de adição de pokemons no banco de dados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { EmailVerificationComponent } from './pages/email-verification/email-verification.component';
 import { InitialPageComponent } from './pages/initial-page/initial-page.component';
+import { AddPokemonComponent } from './pages/add-pokemon/add-pokemon.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'email-verification', component: EmailVerificationComponent},
-  {path: 'initial-page', component: InitialPageComponent}
+  {path: 'initial-page', component: InitialPageComponent},
+  {path: 'add-pokemon', component: AddPokemonComponent}
 ];
 
 @NgModule({
