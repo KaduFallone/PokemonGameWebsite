1de3a3b5c9c6a2a825063cccbdbb0a3920c2430f
KaduFallone <kadufallone@gmail.com>
2026-02-09 08:12:35 -0300

    feat: adiciona jogabilidade á pagina de capturar pokemon

--- src/app/pages/capture-pokemon/capture-pokemon.component.ts
+++ src/app/pages/capture-pokemon/capture-pokemon.component.ts
@@ -2,6 +2,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
 import { FirebaseApiComponent } from '../../tools/firebase-api/firebase-api.component';
 import * as Chance from 'chance';
+import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 
 @Component({
   selector: 'app-capture-pokemon',
@@ -10,28 +12,34 @@ import * as Chance from 'chance';
 })
 export class CapturePokemonComponent implements OnInit {
   path = "Pokemons"
+  pathTrainerDex = "TrainerDex"
+  trainerId = "";
   chance = new Chance.Chance();
-
+  isCapturing: boolean = false;
   pokemonsList: pokeData[] = [];
   sortedePokemon: pokeData | undefined;
 
+  auth = new FirebaseTSAuth();
+  firestore = new FirebaseTSFirestore();
 
   constructor(
     private snackBar: MatSnackBar,
     private api: FirebaseApiComponent
-  ) { }
-
-  ngOnInit(): void {
+  ) { 
+    
   }
 
-  onCaptureClick(){
+  ngOnInit(): void {
+    this.loadPokemons();
+  }
+
+  loadPokemons(){
     this.api.get(this.path, (docsBrutos)=> {
       let tempList: pokeData[]=[];
-
+  
       docsBrutos.forEach((doc: any) => {
         const dados = doc.data();
-        let tempList: pokeData[] =[];
-
+  
         tempList.push(
           {
             id: doc.id,
@@ -44,21 +52,63 @@ export class CapturePokemonComponent implements OnInit {
       this.pokemonsList = tempList;
       console.log("Pokemons carregados:", this.pokemonsList.length);
     });
-    
+  }
+
+  onCaptureClick(){
     if(this.pokemonsList.length === 0){
-    this.snackBar.open("Carregando pokemosn do DB", "OKAY", {duration: 3000});
+      this.snackBar.open("Procurando por pokemons...", "OKAY", {duration: 3000});
+      this.loadPokemons();
       return;
     }
 
-    const pokeIds = this.pokemonsList.map(p => p.id);
-    const captureChance = this.pokemonsList.map(p => p.percentCapture);
+    this.isCapturing = true;
+    this.sortedePokemon = undefined;
 
-    const IdWinner = this.chance.weighted(pokeIds, captureChance);
-
-    this.sortedePokemon = this.pokemonsList.find(p => p.id === IdWinner);
-    console.log("Pokemon capturado:", this.sortedePokemon?.name);
+    setTimeout(() => {
+      const pokeIds = this.pokemonsList.map(p => p.id);
+      const captureChance = this.pokemonsList.map(p => p.percentCapture);
   
-    //this.api.addDB();
+      const IdWinner = this.chance.weighted(pokeIds, captureChance);
+  
+      this.sortedePokemon = this.pokemonsList.find(p => p.id === IdWinner);
+      console.log("Pokemon capturado:", this.sortedePokemon?.name);
+      
+      this.isCapturing = false;
+
+      if(this.sortedePokemon){
+        this.snackBar.open(`Você capturou um ${this.sortedePokemon.name}`, "OKAY", {duration: 3000 });
+
+        this.addPokemon_to_TrainerDex();
+      } 
+      console.log(this.pokemonsList);
+    }, 5000);
+
+  }
+
+  
+  addPokemon_to_TrainerDex(){
+
+    const userId = this.auth.getAuth().currentUser?.uid;
+
+    if(!userId || !this.sortedePokemon) return;
+
+    this.firestore.create(
+      {
+        path:["Users", userId, this.pathTrainerDex],
+        data: {
+          pokeName: this.sortedePokemon.name,
+          imgUrl: this.sortedePokemon.imgUrl,
+          pokeId: this.sortedePokemon.id,
+          captureDate: Date.now()
+        },
+        onComplete: (docId) => {
+          this.snackBar.open("Pokemon enviado para seu PC", "OKAY", {duration: 3000})
+        },
+        onFail: (err) => {
+          this.snackBar.open("O pokemon escapou","OKAY", {duration: 3000})
+        }
+      }
+    )
   }
 
 }
