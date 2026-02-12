0476b9196414bcd96f353f215755270069301ed2
KaduFallone <kadufallone@gmail.com>
2026-02-12 10:11:54 -0300

    feat: add funcionalidade aos potões de porximo e anterior da pokedex

--- src/app/pages/poke-dex/poke-dex.component.ts
+++ src/app/pages/poke-dex/poke-dex.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 //variaveis globais
 
@@ -15,16 +16,18 @@ export class PokeDexComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private snackBar: MatSnackBar
   ) { }
 
   pokemonsName: string = "";
   pokemonsId: string | number = "";
   pokemonsImg: string = "";
+  currentPokemonId: number = 1;
 
   ngOnInit(): void {
     const user = this.auth.getAuth().onAuthStateChanged((user) => {
       if(user){
-        this.renderPokemon('arceus')
+        this.renderPokemon('1')
       }
       else{
         this.router.navigate(['/login'])
@@ -35,23 +38,31 @@ export class PokeDexComponent implements OnInit {
    async fetchPokemon ( pokemon: string | number ){
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     return APIResponse.status === 200 ? await APIResponse.json(): null;
+
+    
   }
 
     
   
 
   async renderPokemon (pokemon: string|number){
+    this.pokemonsName = "Carregando...";
+    this.pokemonsId = "";
+    this.pokemonsImg = "";
+
     const data = await this.fetchPokemon(pokemon);
 
     if(data){
       this.pokemonsName = data.name;
       this.pokemonsId = data.id;
+      this.currentPokemonId = data.id;
       this.pokemonsImg = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     }
     else{
-      this.pokemonsName = "Not found";
-      this.pokemonsId = "";
+      this.pokemonsName = "não encontrado";
+      this.pokemonsId = "???";
       this.pokemonsImg = "";
+      this.snackBar.open('Pokemon não encontrado', 'OKAY', {duration:3000});
     }
 
   }
@@ -62,7 +73,17 @@ export class PokeDexComponent implements OnInit {
     this.renderPokemon(value.toLocaleLowerCase());
   }
 
-  
+  onPrevClick(){
+    if(this.currentPokemonId > 1){
+      this.currentPokemonId--;
+      this.renderPokemon(this.currentPokemonId);
+    }
+  }
+
+  onNextClick(){
+    this.currentPokemonId++;
+    this.renderPokemon(this.currentPokemonId);
+  }
   
 
 }
